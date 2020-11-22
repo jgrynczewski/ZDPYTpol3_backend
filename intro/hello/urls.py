a7c256572cb8f98c669c95b0348fe5489f9f7c4c
Jerzy <jgrynczewski@gmail.com>
2020-11-22 14:38:49 +0100

    Intro version 2

---
+++ intro/hello/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.hello),
+    path('dwa/', views.hello2),
+    path('adam/', views.adam),
+    path('ewa/', views.ewa),
+    path('isitnewyear/', views.new_year),
+    path('name/<name>', views.name2),
+    path('<str:name>', views.name),
+]
--- intro/intro/urls.py
+++ intro/intro/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-    path('hello2/', views.hello2),
+    path('hello/', include('hello.urls'))
 ]
